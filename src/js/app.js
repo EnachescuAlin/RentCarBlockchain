b94156e33dc70e6f2e06e7d37d08bf8cd6971ae2
GeorgeCernat <george.constantin.cernat@ibm.com>
2019-04-07 01:15:25 +0300

    fixes

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,8 @@ App = {
     web3Provider: null,
     contracts: {},
 
+    account: "",
+
     init: function () {
         return App.initWeb3();
     },
@@ -28,6 +30,20 @@ App = {
             // Set the provider for our contract
             App.contracts.Renting.setProvider(App.web3Provider);
             // Use our contract to retrieve value data
+
+            web3.eth.getAccounts(function (error, accounts) {
+                if (error) {
+                    console.log(error);
+                }
+
+                var wrapperAccounts = $('#wrapperAccounts');
+
+                accounts.forEach(account => {
+                    wrapperAccounts.append(`<option value=${account}> ${account} </option>`);
+                })
+
+            })
+
             App.getCars();
         });
         return App.bindEvents();
@@ -51,114 +67,161 @@ App = {
 
     getCars: function () {
         var carsInstance;
-        web3.eth.getAccounts(function (error, accounts) {
-            if (error) {
-                console.log(error);
-            }
-            var account = accounts[1];
-            App.contracts.Renting.deployed().then(function (instance) {
-                carsInstance = instance;
 
-                carsInstance.getNumCars.call().then(function (numCars) {
-                    var wrapperCars = $('#wrapperCars');
-                    wrapperCars.empty();
-                    var proposalCar = $('#proposalCar');
-                    for (var i = 0; i < numCars; i++) {
-                        carsInstance.getCar.call(i).then(function (data) {
-							var idx = i;
-							
-                            proposalCar.find('.panel-title').text(data[0]);
-                            proposalCar.find('.price').text(data[1]);
-                            proposalCar.find('.available').text(data[2]);
-                            proposalCar.find('.btn-vote').attr('data-car', idx);
-							proposalCar.find('.btn-vote').attr('disabled', false);
-							proposalCar.find('.btn-rent').attr('data-car', idx);
-							proposalCar.find('.btn-free').attr('data-car', idx);
+        // web3.eth.getAccounts(function (error, accounts) {
+        //     if (error) {
+        //         console.log(error);
+        // 	}
 
-							if(data[3] == true) {
-								proposalCar.find('.btn-rent').attr('disabled', false);
-								proposalCar.find('.btn-free').attr('disabled', true);
-							}
-							else {
-								if(data[4] == data[0]) {
-									proposalCar.find('.btn-free').attr('disabled', false);
-									proposalCar.find('.btn-rent').attr('disabled', true);
-								}
+        // var account = accounts[1];
 
-								proposalCar.find('.btn-rent').attr('disabled', true);
-								proposalCar.find('.btn-free').attr('disabled', true);
-							}
+        App.contracts.Renting.deployed().then(function (instance) {
+            carsInstance = instance;
 
-                            wrapperCars.append(proposalCar.html());
-                        }).catch(function (err) {
-                            console.log(err.message);
-                        });
-                    }
-                }).catch(function (err) {
-                    console.log(err.message);
-                });
+            carsInstance.getNumCars.call().then(function (numCars) {
+                var wrapperCars = $('#wrapperCars');
+                wrapperCars.empty();
+				var proposalCar = $('#proposalCar');
+				
+                for (var i = 0; i < numCars; i++) {
+                    carsInstance.getCar.call(i).then(function (data) {
+                    // carsInstance.getCar(i, {
+                    //     from: account,
+                    //     gas: 1000000
+                    // }).then(function (data) {
+                        var idx = i;
+
+                        proposalCar.find('.panel-title').text(data[1]);
+                        proposalCar.find('.price').text(data[2]);
+                        proposalCar.find('.available').text(data[3]);
+                        proposalCar.find('.btn-rent').attr('data-car', idx);
+                        proposalCar.find('.btn-free').attr('data-car', idx);
+
+                        if (data[3] == true) {
+                            proposalCar.find('.btn-rent').attr('disabled', false);
+                            proposalCar.find('.btn-free').attr('disabled', true);
+                        } else {
+                            if (data[4] == account) {
+                                proposalCar.find('.btn-free').attr('disabled', false);
+                                proposalCar.find('.btn-rent').attr('disabled', true);
+                            }
+
+                            proposalCar.find('.btn-rent').attr('disabled', true);
+                            proposalCar.find('.btn-free').attr('disabled', true);
+                        }
+
+                        wrapperCars.append(proposalCar.html());
+                    }).catch(function (err) {
+                        console.log(err.message);
+                    });
+                }
+            }).catch(function (err) {
+                console.log(err.message);
             });
-        });
+        }).catch(function (err) {
+			console.log(err.message);
+		});
+        // });
         $('button').button('reset');
     },
 
     handleAddCar: function (event) {
-		event.preventDefault();
-		
+        event.preventDefault();
+
         var carInstance;
-		var value = $('.input-value').val();
-		
-        web3.eth.getAccounts(function (error, accounts) {
-            if (error) {
-                console.log(error);
-			}
-			
-			var account = accounts[1];
-			
-            App.contracts.Renting.deployed().then(function (instance) {
-                carInstance = instance;
-                return carInstance.addCar(value, 1, {
-                    from: account, gas: 1000000
-                });
-            }).then(function (result) {
-                var event = carInstance.CreatedCarEvent();
-                App.handleEvent(event);
-                $('.input-value').val(''); // clean input
-            }).catch(function (err) {
-                console.log(err.message);
-                $('button').button('reset');
+        account = $('#wrapperAccounts').val();
+        var value = $('.input-value').val();
+        var price = $('.input-price').val();
+
+        console.log(account, value, price)
+
+        // web3.eth.getAccounts(function (error, accounts) {
+        //     if (error) {
+        //         console.log(error);
+        // 	}
+
+        // var account = accounts[1];
+
+        App.contracts.Renting.deployed().then(function (instance) {
+            carInstance = instance;
+
+            return carInstance.addCar(value, price, {
+                from: account,
+                gas: 1000000
             });
+        }).then(function (result) {
+            var event = carInstance.CreatedCarEvent();
+            App.handleEvent(event);
+
+            $('.input-value').val(''); // clean input
+            $('.input-price').val(''); // clean price
+        }).catch(function (err) {
+            console.log(err.message);
+            $('button').button('reset');
         });
+        // });
     },
 
-    handleAddVote: function (event) {
-		event.preventDefault();
-		
-        var voteInstance;
-        var voteValue = parseInt($(event.target).data('vote'));
-		var proposalInt = parseInt($(event.target).data('proposal'));
-		
-        web3.eth.getAccounts(function (error, accounts) {
-            if (error) {
-                console.log(error);
-			}
-			
-			var account = accounts[1];
-			
-            App.contracts.Renting.deployed().then(function (instance) {
-                voteInstance = instance;
-                return voteInstance.vote(proposalInt, voteValue, {
-                    from: account,
-                    gas: 1000000
-                });
-            }).then(function (result) {
-                var event = voteInstance.CreatedVoteEvent();
-                App.handleEvent(event);
-            }).catch(function (err) {
-                console.log(err.message);
-                $('button').button('reset');
+    handleRentCar: function (event) {
+        event.preventDefault();
+
+        var carInstance;
+        var carInt = parseInt($(event.target).data('car'));
+
+        // web3.eth.getAccounts(function (error, accounts) {
+        // if (error) {
+        //     console.log(error);
+        // }
+
+        // var account = accounts[1];
+
+        App.contracts.Renting.deployed().then(function (instance) {
+            carInstance = instance;
+
+            return carInstance.rentCar(carInt, {
+                from: account,
+                gas: 1000000
             });
+        }).then(function (result) {
+            var event = carInstance.CreatedRentEvent();
+            App.handleEvent(event);
+
+        }).catch(function (err) {
+            console.log(err.message);
+            $('button').button('reset');
         });
+        // });
+    },
+
+    handleFreeCar: function (event) {
+        event.preventDefault();
+
+        var carInstance;
+        var carInt = parseInt($(event.target).data('car'));
+
+        // web3.eth.getAccounts(function (error, accounts) {
+        // if (error) {
+        //     console.log(error);
+        // }
+
+        // var account = accounts[1];
+
+        App.contracts.Renting.deployed().then(function (instance) {
+            carInstance = instance;
+
+            return carInstance.freeCar(carInt, {
+                from: account,
+                gas: 1000000
+            });
+        }).then(function (result) {
+            var event = carInstance.CreatedCarEvent();
+            App.handleEvent(event);
+
+        }).catch(function (err) {
+            console.log(err.message);
+            $('button').button('reset');
+        });
+        // });
     },
 
     handleEvent: function (event) {
